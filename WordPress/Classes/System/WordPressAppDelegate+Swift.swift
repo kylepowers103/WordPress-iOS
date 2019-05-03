6a848e9dcb84287073ccae58c0e5596cc162210a
Jeremy Massel <jeremy.massel@gmail.com>
2019-05-03 16:17:23 -0600

    Improve Sentry tags

--- WordPress/Classes/System/WordPressAppDelegate+Swift.swift
+++ WordPress/Classes/System/WordPressAppDelegate+Swift.swift
@@ -34,11 +34,7 @@ extension WordPressAppDelegate {
     }
 
     @objc func configureCrashLogging() {
-        #if DEBUG
-            return
-        #else
-            WPCrashLogging.start()
-        #endif
+        WPCrashLogging.start()
     }
 
     @objc func configureHockeySDK() {
